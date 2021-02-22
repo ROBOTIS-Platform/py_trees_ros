ab33f933bb3341ba03365150118b41ebf80e9270
Darby Lim <thlim@robotis.com>
2021-02-22 13:16:35 +0900

    Added resource folder

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
         exclude=['doc*', 'tests*', 'graveyard*', 'scripts*']
     ),
     data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     package_data={},
